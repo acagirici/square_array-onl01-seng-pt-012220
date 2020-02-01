e19eb84890bcf7d970a30d7351ffbed26c486bd6
Arif Cagirici <acagirici23@gmail.com>
2020-02-01 01:09:29 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,9 @@
-def square_array(array)
-  numbers = [1,2,3]
-  numbers.each do |number|
-  end
+def square_array(numbers)
+new_numbers = []
+numbers.each do |num|
+
+new_numbers << (num ** 2)
+
+end
+return new_numbers
 end
